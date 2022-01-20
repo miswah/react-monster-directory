b7f1c0c10c39a37120607bbdc5562aaa47d53f58
miswah <miswah@monkhub.com>
2022-01-20 15:04:46 -0800

    (enh): assign card-list component to app coponent

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { CardList } from "./components/card-list.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((body) => this.setState({ monsters: body }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList monsters={this.state.monsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
